498117458c4dce7cb1a5892b2d660b58ac8dbae5
xxxFRIARxxx <iiifriariii@gmail.com>
2023-03-01 08:42:41 -0600

    Calmed down exhibition gamemode/gamestate spam

--- SaltyStateMachine.py
+++ SaltyStateMachine.py
@@ -37,7 +37,6 @@ thread.start()
 while True:
     the_json = my_thing.get_json()
     my_parser = SaltyJsonParser(the_json)
-    # balance = interactor.get_balance()
     game_mode = my_parser.get_gameMode()
     game_state = my_parser.get_gamestate()
 
@@ -61,10 +60,8 @@ while True:
                 p1_probability = bettor.probability_of_p1_win(p1DB_ratings.mu, p1DB_ratings.sigma, p2DB_ratings.mu, p2DB_ratings.sigma)
                 new_match = 1
                 my_socket.find_winstreak = True
-                # TODO: The line below makes this program go into hyperdrive - Is this bad?
                 bettor.set_balance(interactor.get_balance()) 
                 my_parser.gameMode_printer()
-                # bettor.set_balance(balance) # Sets and displays current balance of your account.
                 print(f"Player 1:   Mu = {p1DB_ratings.mu}  Sigma = {p1DB_ratings.sigma}  Winstreak from DB = {p1DB_streak}")
                 print(f"Player 2:   Mu = {p2DB_ratings.mu}  Sigma = {p2DB_ratings.sigma}  Winstreak from DB = {p2DB_streak}")
                 # TODO:  Include data regression to help compose bet here.
@@ -75,12 +72,12 @@ while True:
                     new_match = 2
                     game_time.timer_start()
                     my_parser.gameMode_printer()  
-        elif (game_state == '1') or ((game_state == '2')): 
+        elif (game_state == '1') or (game_state == '2'): 
             if (first_run == False):
                 if (new_match == 2):
-                    my_parser.gameMode_printer()
                     new_match = 0
                     game_time.timer_snapshot()
+                    my_parser.gameMode_printer()
                     ratings_to_db = bettor.update_ranking_after(game_state, p1DB_ratings, p2DB_ratings) # Updates ratings after the match.
                     my_socket.adjust_winstreak(my_parser.set_p1winstatus(), my_parser.set_p2winstatus(), thread.value1, thread.value2)
                     my_socket.adjust_tier(thread.value3)
@@ -88,7 +85,19 @@ while True:
                     recorder.record_match(my_parser.get_p1name(),my_parser.get_p1odds(), my_parser.set_p1winstatus(), my_parser.get_p2name(), my_parser.get_p2odds(), my_parser.set_p2winstatus(), my_socket.adj_p1winstreak, my_socket.adj_p2winstreak, my_socket.adj_p1_tier, my_socket.adj_p2_tier, ratings_to_db[0].mu, ratings_to_db[0].sigma, ratings_to_db[1].mu, ratings_to_db[1].sigma, game_time.snapshot, bettor.outcome, my_parser.is_tourney())
 
     elif game_mode == "Exhibition":
-        my_parser.gameMode_printer()
+        if (game_state == "open"):
+            if (new_match == 0):
+                my_parser.gameMode_printer()
+                new_match = 1
+        elif (game_state == "locked"):
+            if (new_match == 1):
+                my_parser.gameMode_printer()
+                new_match = 2
+        elif (game_state == "1") or (game_state == "2"):
+            if (new_match == 2):
+                my_parser.gameMode_printer()
+                new_match = 0
+        
     #     if (first_run == False):
     #         if (new_match == 2):
                 # if (my_parser.get_matches_remaining() == 25) and (game_state in [1,2]):
@@ -99,18 +108,6 @@ while True:
                 #     my_socket.adjust_tier(thread.value3)
                 #     bettor.bet_outcome(my_parser.get_p1name(), my_parser.get_p2name(), game_state)
                 #     recorder.record_match(my_parser.get_p1name(),my_parser.get_p1odds(), my_parser.set_p1winstatus(), my_parser.get_p2name(), my_parser.get_p2odds(), my_parser.set_p2winstatus(), my_socket.adj_p1winstreak, my_socket.adj_p2winstreak, my_socket.adj_p1_tier, my_socket.adj_p2_tier, ratings_to_db[0].mu, ratings_to_db[0].sigma, ratings_to_db[1].mu, ratings_to_db[1].sigma, game_time.snapshot, bettor.outcome, my_parser.is_tourney())
-
-
-# TODO: EXPLOSION:
-                # Traceback (most recent call last):
-                #   File "e:\Python Scripts\SaltyBot\SaltyStateMachine.py", line 41, in <module>
-                #     game_mode = my_parser.get_gameMode()
-                #   File "e:\Python Scripts\SaltyBot\SaltyParser.py", line 91, in get_gameMode
-                #     if self.is_exhib() is True:
-                #   File "e:\Python Scripts\SaltyBot\SaltyParser.py", line 46, in is_exhib
-                #     exhib_split = self.json_dict["remaining"].split(' ')[1]
-                # TypeError: 'NoneType' object is not subscriptable
-
 
 # TODO: Last match of tourney still doesn't record:
                 # Currently in Tournament with 1 matches remaining.  Game state is locked.

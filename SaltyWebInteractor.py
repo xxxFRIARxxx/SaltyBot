83b74edbb56d80442edcdac1f81647f9dda1c944
xxxFRIARxxx <iiifriariii@gmail.com>
2023-03-06 15:14:48 -0600

    Cleanup

--- SaltyWebInteractor.py
+++ SaltyWebInteractor.py
@@ -9,20 +9,11 @@ load_dotenv()
 URL_SIGNIN = 'https://www.saltybet.com/authenticate?signin=1'
 URL_BET = 'https://www.saltybet.com/ajax_place_bet.php'
 
-# cookies = {
-#     '_ga': 'GA1.2.410504609.1668662511',
-#     '_gid': 'GA1.2.1665887669.1675437246',
-#     # 'PHPSESSID': '6ucnbid62ogacdm504l7chioq0',
-# }
-
-
-
 headers = {
     'authority': 'www.saltybet.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
-    # 'cookie': 'PHPSESSID=6ucnbid62ogacdm504l7chioq0',
     'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',

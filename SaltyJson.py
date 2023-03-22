7d423841b4ce4b9dad4290bedd3902bc322ff526
zappybibby <calcinerdin@protonmail.com>
2023-03-22 14:20:26 -0400

    revert adding timeout
    Apparently this can only be done per request, not session.

--- SaltyJson.py
+++ SaltyJson.py
@@ -9,7 +9,6 @@ class SaltyJson():
         self.session.headers.update({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
             "Accept": "application/json"})
-        self.session.timeout = 5
 
     def get_json(self):
         try:

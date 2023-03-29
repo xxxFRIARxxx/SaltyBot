76525299715b544a226822d514e8a7811b398119
xxxFRIARxxx <iiifriariii@gmail.com>
2023-03-29 10:29:13 -0500

    Introducted new method of predicting winner

---
+++ SaltyTest.py
@@ -0,0 +1,12 @@
+mylist = [None, 2, 1]
+
+if not all(mylist):
+    print("at least 1 is false")
+else:
+    print("All are true")
+
+# if not any(mylist):
+#     print("All are false")
+# else:
+#     print("At least 1 is true")
+

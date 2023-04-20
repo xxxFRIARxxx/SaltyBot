f19a9f3392c3ac5e9c5923d46c944f70ff8b5e25
zappybibby <calcinerdin@protonmail.com>
2023-04-20 16:05:42 -0400

    exponential backoff, random user agent, remove json_is_valid

--- SaltyJson.py
+++ SaltyJson.py
@@ -1,40 +1,31 @@
 import requests
-import time
-import json
-
-from tenacity import retry, stop_after_attempt, wait_fixed, RetryError
+from fake_useragent import UserAgent
+from tenacity import retry, stop_after_attempt, wait_exponential_jitter, TryAgain
 
 
 class SaltyJson:
     def __init__(self):
         self.url = "https://www.saltybet.com/state.json"
-        self.session = requests.Session()
-        self.session.headers.update({"User-Agent": "Mozilla/5.0", "Accept": "application/json"})
+        self.session = self.create_session()
 
-    @retry(stop=stop_after_attempt(5), wait=wait_fixed(1),
+    def create_session(self):
+        ua = UserAgent()
+        session = requests.Session()
+        headers = {'User-Agent': f"'{ua.random}'"}
+        session.headers.update(headers)
+        return session
+
+    @retry(stop=stop_after_attempt(5), wait=wait_exponential_jitter(initial=1, max=6, jitter=1),
            retry_error_callback=lambda r: print(f"Error: {r}"))
     def get_json(self):
-        self.response = self.session.get(self.url)
-        if self.response.status_code != 200:
-            print(f"Received {self.response.status_code} status code, retrying... ")
+        response = self.session.get(self.url)
+        self.handle_response(response)
+
+    def handle_response(self, response):
+        if response.status_code != 200:
+            print(f"Received {response.status_code} status code, retrying... ")
             raise requests.exceptions.ConnectionError
-        elif not self.response.text:
-            print("Blank response received, retrying...")
-            raise RetryError
-        elif self.json_is_valid(self.response.json()) is True:
-            return self.response.json()
+        elif response.text:
+            return response.json()
         else:
-            raise RetryError
-
-    def json_is_valid(self, json_data):
-        try:
-            required_keys = ["p1name", "p2name", "p1total", "p2total", "status", "remaining"]
-
-            for key in required_keys:
-                if key in json_data and json_data[key]:
-                    return True
-                print(f"Invalid data: key '{key}' is missing or has a falsey value")
-                return False
-        except Exception as e:
-            print(f"Error: {e}")
-            return False
+            raise TryAgain

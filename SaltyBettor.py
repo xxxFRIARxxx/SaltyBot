e2756d8ed9ebe1fc4507ba988588c59be13bdf4e
xxxFRIARxxx <iiifriariii@gmail.com>
2023-04-29 08:59:12 -0500

    Lazyness Update

--- SaltyBettor.py
+++ SaltyBettor.py
@@ -94,8 +94,8 @@ class SaltyBettor():
         elif p1_probability > .5:  
             if (p1mu - p1sigma) > (p2mu + p2sigma): 
                 self.predicted_w = player1_dict[p1_json]
-            elif (p1_odds_avg > (p2_odds_avg * 2)):
-                self.predicted_w = player1_dict[p1_json]
+            # elif (p1_odds_avg > (p2_odds_avg * 2)):
+            #     self.predicted_w = player1_dict[p1_json]
             # elif p1sigma < p2sigma:
             #     self.predicted_w = player1_dict[p1_json]
             # elif p1DB_streak > p2DB_streak:
@@ -105,8 +105,8 @@ class SaltyBettor():
         elif p1_probability < .5:
             if (p2mu - p2sigma) > (p1mu + p1sigma): 
                 self.predicted_w = player2_dict[p2_json]
-            elif (p2_odds_avg > (p1_odds_avg * 2)):
-                self.predicted_w = player2_dict[p2_json]
+            # elif (p2_odds_avg > (p1_odds_avg * 2)):
+            #     self.predicted_w = player2_dict[p2_json]
             # elif p2sigma < p1sigma:
             #     self.predicted_w = player2_dict[p2_json]
             # elif p2DB_streak > p1DB_streak:
@@ -139,35 +139,36 @@ class SaltyBettor():
         return self.predicted_w
 
     def kelly_bet(self, p1_probability, p1_odds_avg, p2_odds_avg, balance, predicted_winner, game_mode):
+        # self.suggested_wager = 1
         self.suggested_wager = 1
-        q = 1-p1_probability
-        if not all([p1_odds_avg, p2_odds_avg]):
-            b = 1
-        elif p1_odds_avg > p2_odds_avg:
-            b = p1_odds_avg
-        elif p2_odds_avg > p1_odds_avg:
-            b = p2_odds_avg
-        # elif abs(p1_odds_avg - p2_odds_avg) == 0:
-        #     b = 5
-        # elif abs(p1_odds_avg - p2_odds_avg) < 1:
-        #     b = 10
-        else:
-            b = 1
-        # b = 1
-        # b = p1_probability/(1-p1_probability)
-        fraction = p1_probability-(q/b)
-        k_suggest = abs(.05*(fraction*balance))
-        if (game_mode == "Tournament") and (self.balance < 20000):
-            self.suggested_wager = self.balance
-        elif (game_mode == "Matchmaking") and (self.balance < 10000):
-            self.suggested_wager = self.balance
-        elif predicted_winner is None:
-            self.suggested_wager = 1
-        elif (predicted_winner != None) and (game_mode != "Tournament"):
-            if p1_probability != .5:
-                self.suggested_wager = decimal.Decimal(k_suggest).quantize(decimal.Decimal('0'), rounding=decimal.ROUND_UP)          
-            elif (p1_probability == .5):
-                self.suggested_wager = 1              
+        # q = 1-p1_probability
+        # if not all([p1_odds_avg, p2_odds_avg]):
+        #     b = 1
+        # elif p1_odds_avg > p2_odds_avg:
+        #     b = p1_odds_avg
+        # elif p2_odds_avg > p1_odds_avg:
+        #     b = p2_odds_avg
+        # # elif abs(p1_odds_avg - p2_odds_avg) == 0:
+        # #     b = 5
+        # # elif abs(p1_odds_avg - p2_odds_avg) < 1:
+        # #     b = 10
+        # else:
+        #     b = 1
+        # # b = 1
+        # # b = p1_probability/(1-p1_probability)
+        # fraction = p1_probability-(q/b)
+        # k_suggest = abs(.05*(fraction*balance))
+        # if (game_mode == "Tournament") and (self.balance < 20000):
+        #     self.suggested_wager = self.balance
+        # elif (game_mode == "Matchmaking") and (self.balance < 10000):
+        #     self.suggested_wager = self.balance
+        # elif predicted_winner is None:
+        #     self.suggested_wager = 1
+        # elif (predicted_winner != None) and (game_mode != "Tournament"):
+        #     if p1_probability != .5:
+        #         self.suggested_wager = decimal.Decimal(k_suggest).quantize(decimal.Decimal('0'), rounding=decimal.ROUND_UP)          
+        #     elif (p1_probability == .5):
+        #         self.suggested_wager = 1              
         return self.suggested_wager
 
     def format_bet(self, predicted_winner, suggested_bet):

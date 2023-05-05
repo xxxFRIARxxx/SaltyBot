0d6d2c8353c6f017cc447c693f8cb91cd798ea52
xxxFRIARxxx <iiifriariii@gmail.com>
2023-05-04 20:31:45 -0500

    Reverted back to working condition

--- SaltyBettor.py
+++ SaltyBettor.py
@@ -1,3 +1,4 @@
+import webbrowser
 import math
 from trueskill import Rating, rate_1vs1
 from statistics import NormalDist
@@ -5,10 +6,6 @@ import decimal
 
 class SaltyBettor():
     def __init__(self):
-        self.predicted_w = None
-        self.suggested_player = None
-        self.suggested_wager = 1
-        self.upset_bet = False
         self.balance = 0
         self.outcome = 0
         self.old_balance = 0
@@ -16,27 +13,29 @@ class SaltyBettor():
     def set_balance(self, balance_value):
         self.balance = balance_value
 
-    def bet_outcome_amount(self):
+    def bet_outcome_amount(self, gs_lies):
         if self.old_balance == 0:
             self.old_balance = self.balance
-
-        elif self.old_balance < self.balance:
+        if gs_lies == True:
+            pass
+        elif (self.old_balance < self.balance):
             balance_diff = self.balance - self.old_balance
             self.old_balance = self.balance
             print(f"Last bet: You won ${balance_diff:,}")
-        elif self.old_balance > self.balance:
+        elif (self.old_balance > self.balance):
             balance_diff = self.old_balance - self.balance
             self.old_balance = self.balance
             print(f"Last bet: You lost ${balance_diff:,}")
 
     def bet_outcome(self, p1_json, p2_json, game_state):
-        player1_dict = {p1_json: 'player1'}
-        player2_dict = {p2_json: 'player2'}
+        player1_dict = {p1_json:'player1'}
+        player2_dict = {p2_json:'player2'}
         if game_state == "1":
             winner = player1_dict[p1_json]
         elif game_state == "2":
             winner = player2_dict[p2_json]
         else:
+            webbrowser.open("https://www.saltybet.com/state.json")
             print(game_state)
             print('This prints when no player has won the match.  (Draw?) This message comes from bettor -> bet_outcome')       
         if self.suggested_player['selectedplayer'] == winner:
@@ -46,14 +45,14 @@ class SaltyBettor():
             print('You lost the bet.')
             self.outcome = 0
 
-    def probability_of_p1_win(self, p1mu, p1sigma, p2mu, p2sigma):
+    def probability_of_p1_win(self, p1mu, p1sigma, p2mu, p2sigma): # Uses the cumulative distribution function of a normal distribution to determine probability of p1 winning.
         prob_P1 = None
-        deltaMu = (p1mu - p2mu)
-        sumSigma = (p1sigma ** 2) + (p2sigma ** 2)
+        deltaMu = (p1mu - p2mu)                   
+        sumSigma = (p1sigma**2) + (p2sigma**2)  
         playerCount = 2
-        beta = 4.166666666666667  # Sigma / 2
-        denominator = math.sqrt(playerCount * (beta * beta) + sumSigma)
-        prob_P1 = NormalDist().cdf(deltaMu / denominator)
+        beta = 4.166666666666667           # Sigma / 2                             
+        denominator = math.sqrt(playerCount * (beta * beta) + sumSigma)   
+        prob_P1 = NormalDist().cdf(deltaMu/denominator)
         return prob_P1
 
     # def predicted_winner(self, p1sigma, p2sigma, p1_probability, p1_json, p2_json, p1DB_streak, p2DB_streak): # Predicts winner through probability of winning, then through difference in Sigma values, then through streaks.
@@ -86,33 +85,36 @@ class SaltyBettor():
     
     def prediciton(self, p1sigma, p2sigma, p1mu, p2mu, p1_probability, p1_json, p2_json, p1DB_streak, p2DB_streak, p1_odds_avg, p2_odds_avg): # Mu != 25, Mu +/- sigma, odds avg, sigma diff, streak diff
         self.predicted_w = None
-        player1_dict = {p1_json: 'player1'}
-        player2_dict = {p2_json: 'player2'}
-
-        if p1mu == 25 or p2mu == 25:  # If p1 or p2 has no recorded matches
+        player1_dict = {p1_json:'player1'}
+        player2_dict = {p2_json:'player2'} 
+        if p1mu == 25 or p2mu == 25:
+            self.predicted_w == None
+        if not all([p1_odds_avg, p2_odds_avg, p1DB_streak, p2DB_streak]): # If any odds or streak is None or False
             self.predicted_w = None
-        elif p1_probability > .5:  
-            if (p1mu - p1sigma) > (p2mu + p2sigma): 
-                self.predicted_w = player1_dict[p1_json]
+        elif p1_probability > .5:
+            self.predicted_w = player1_dict[p1_json]
+            # if (p1mu - p1sigma) > (p2mu + p2sigma): 
+            #     self.predicted_w = player1_dict[p1_json]
             # elif (p1_odds_avg > (p2_odds_avg * 2)):
             #     self.predicted_w = player1_dict[p1_json]
             # elif p1sigma < p2sigma:
             #     self.predicted_w = player1_dict[p1_json]
             # elif p1DB_streak > p2DB_streak:
             #     self.predicted_w = player1_dict[p1_json]
-            else:
-                self.predicted_w = None
+            # else:
+            #     self.predicted_w = None
         elif p1_probability < .5:
-            if (p2mu - p2sigma) > (p1mu + p1sigma): 
-                self.predicted_w = player2_dict[p2_json]
+            self.predicted_w = player2_dict[p2_json]
+            # if (p2mu - p2sigma) > (p1mu + p1sigma): 
+            #     self.predicted_w = player2_dict[p2_json]
             # elif (p2_odds_avg > (p1_odds_avg * 2)):
             #     self.predicted_w = player2_dict[p2_json]
             # elif p2sigma < p1sigma:
             #     self.predicted_w = player2_dict[p2_json]
             # elif p2DB_streak > p1DB_streak:
             #     self.predicted_w = player2_dict[p2_json]
-            else:
-                self.predicted_w = None
+            # else:
+            #     self.predicted_w = None
         elif p1_probability == .5 :
             if p1_odds_avg > p2_odds_avg:
                 self.predicted_w = player1_dict[p1_json]
@@ -139,27 +141,24 @@ class SaltyBettor():
         return self.predicted_w
 
     def kelly_bet(self, p1_probability, p1_odds_avg, p2_odds_avg, balance, predicted_winner, game_mode):
-        # self.suggested_wager = 1
         self.suggested_wager = 1
-
+        q = 1-p1_probability
         if not all([p1_odds_avg, p2_odds_avg]):
-            p1_odds_avg = 1
-            p2_odds_avg = 1
-        if p1_probability > 0.5:
-            b = 1 / p1_odds_avg
-            q = 1 - p1_probability
-            p = p1_probability
-        elif p1_probability < 0.5:
-            b = 1 / p2_odds_avg
-            q = p1_probability
-            p = abs(p1_probability - 1)
+            b = 1
+        elif p1_odds_avg > p2_odds_avg:
+            b = p1_odds_avg
+        elif p2_odds_avg > p1_odds_avg:
+            b = p2_odds_avg
+        # elif abs(p1_odds_avg - p2_odds_avg) == 0:
+        #     b = 5
+        # elif abs(p1_odds_avg - p2_odds_avg) < 1:
+        #     b = 10
         else:
             b = 1
-            q = 0.5
-            p = 0.5
-
-        k_fraction = ((p * b) - q) / b
-        k_suggest = k_fraction*balance
+        # b = 1
+        # b = p1_probability/(1-p1_probability)
+        fraction = p1_probability-(q/b)
+        k_suggest = abs(.05*(fraction*balance))
         if (game_mode == "Tournament") and (self.balance < 20000):
             self.suggested_wager = self.balance
         elif (game_mode == "Matchmaking") and (self.balance < 10000):
@@ -167,81 +166,38 @@ class SaltyBettor():
         elif predicted_winner is None:
             self.suggested_wager = 1
         elif (predicted_winner != None) and (game_mode != "Tournament"):
-            if p1_probability != .5 and k_fraction > 0:
+            if p1_probability != .5:
                 self.suggested_wager = decimal.Decimal(k_suggest).quantize(decimal.Decimal('0'), rounding=decimal.ROUND_UP)          
             elif (p1_probability == .5):
                 self.suggested_wager = 1              
-
         return self.suggested_wager
 
-    # def kelly_bet(self, p1_probability, p1_odds_avg, p2_odds_avg, balance, predicted_winner, game_mode):
-    #     # self.suggested_wager = 1
-    #     self.suggested_wager = 1
-
-    #     if not all([p1_odds_avg, p2_odds_avg]): # TODO: if p1 odds or p2 odds don't exist in the DB (to provide avgs), set both avgs to 1.  Is this cool?
-    #         p1_odds_avg = 1
-    #         p2_odds_avg = 1
-    #     if p1_probability > 0.5:
-    #         b = 1 / p1_odds_avg # TODO: Why are we using odds average from DB? B is the proportion of the bet gained with a win. $10 on 2-1 oddds, b = $20/$10 = 2.0
-    #         q = 1 - p1_probability
-    #         p = p1_probability
-    #     elif p1_probability < 0.5:
-    #         b = 1 / p2_odds_avg
-    #         q = p1_probability
-    #         p = abs(p1_probability - 1) # TODO:  Isn't this the same as 1 - p1_probability?
-    #     else:
-    #         b = 1
-    #         q = 0.5
-    #         p = 0.5
-
-    #     k_fraction = ((p * b) - q) / b # TODO: ((probability of winning * proportion of bet gained w/ a win) - probability of losing) / proportion of bet gained w/ a win
-    #     k_suggest = k_fraction*balance
-    #     if (game_mode == "Tournament") and (self.balance < 20000):
-    #         self.suggested_wager = self.balance
-    #     # elif (game_mode == "Matchmaking") and (self.balance < 10000):
-    #     #     self.suggested_wager = self.balance
-    #     elif predicted_winner is None:
-    #         self.suggested_wager = 1
-    #     elif (predicted_winner != None) and (game_mode != "Tournament"):
-    #         if p1_probability != .5 and k_fraction > 0:
-    #             self.suggested_wager = decimal.Decimal(k_suggest).quantize(decimal.Decimal('0'), rounding=decimal.ROUND_UP)          
-    #         elif (p1_probability == .5):
-    #             self.suggested_wager = 1              
-
-    #     return self.suggested_wager
-
     def format_bet(self, predicted_winner, suggested_bet):
-        p1name = {'selectedplayer': 'player1'}
-        p2name = {'selectedplayer': 'player2'}
+        self.p1name = {'selectedplayer': 'player1'}
+        self.p2name = {'selectedplayer': 'player2'}   
         self.wager = {'wager': suggested_bet}
-        self.suggested_player = None
+        if predicted_winner is None: # If ratings from the DB are both default thru bettor earlier or they're found and both the same, AND their Sigmas are the same (or don't come back), AND if EITHER P1streak or P2 streak DOESN'T come back from DB OR are the same.
+            self.suggested_player = self.p1name # Red wins in a draw, so this provides a miniscule advantage over random choice of a suggested winner.
+        # if suggested_bet > 20000:
+        #     self.wager["wager"] = 20000
+        if self.p1name["selectedplayer"] == predicted_winner:
+            self.suggested_player = self.p1name
+        elif self.p2name["selectedplayer"] == predicted_winner:
+            self.suggested_player = self.p2name
+        return self.suggested_player | self.wager # Returns in the format neccessary for bet-placement on SaltyBet.com: {:} | {:}
 
-        if predicted_winner is None:
-            self.suggested_player = p1name  # Red wins in a draw, so this provides a minuscule advantage
-        elif p1name["selectedplayer"] == predicted_winner:
-            self.suggested_player = p1name if not self.upset_bet else p2name
-            if self.upset_bet:
-                print("Kelly bet suggests upset bet!")
-        elif p2name["selectedplayer"] == predicted_winner:
-            self.suggested_player = p2name if not self.upset_bet else p1name
-            if self.upset_bet:
-                print("Kelly bet suggests upset bet!")
-
-        # Returns the format necessary for betting on SaltyBet.com: {:} | {:}
-        return self.suggested_player | self.wager
-
-    def set_player_rating(self, db_result):
-        if db_result is None:
+    def set_player_rating(self, db_result):  # Sets player ratings for current match.
+        if db_result is None:  
             self.rating = Rating()
         else:
-            self.rating = Rating(db_result[0], db_result[1])
-        return self.rating
-
-    def update_ranking_after(self, game_state, p1finalinput, p2finalinput):
+            self.rating = Rating(db_result[0],db_result[1])
+        return self.rating # Returns either the default rating if none is found in the DB, or the rating of the selected player from their Mu and Sigma pulled from the DB.
+    
+    def update_ranking_after(self, game_state, p1finalinput, p2finalinput):  # Updates rankings of both players after current match is over.
         if game_state == "1":
             p1final, p2final = rate_1vs1(p1finalinput, p2finalinput)
             print("Player 1 wins!")
         elif game_state == "2":
             p2final, p1final = rate_1vs1(p2finalinput, p1finalinput)
             print("Player 2 wins!")
-        return p1final, p2final
+        return p1final, p2final # Returns final Ratings objects of each player from the current match after completion.  Ratings objects contain updated Mu and Sigma values.  
--- SaltyStateMachine.py
+++ SaltyStateMachine.py
@@ -1,6 +1,4 @@
 import os
-import time
-
 from SaltyJson import SaltyJson
 from SaltyParser import SaltyJsonParser
 from SaltyDatabase import SaltyDatabase
@@ -26,10 +24,13 @@ total_match_time_sec = 0
 
 first_run = True
 exiting_tourney = False
+previous_game_mode = None
+game_state_lies = False
+previous_game_state = None
 p1DB_streak = None
 p2DB_streak = None
 
-thread.start()
+thread.start() 
 
 while True:
     the_json = my_json.get_json()
@@ -37,99 +38,93 @@ while True:
     game_mode = my_parser.get_gamemode()
     game_state = my_parser.get_gamestate()
 
-    if game_mode != 'Exhibition':
-        if game_state == 'open':
-            if new_match == 0:
+
+    #if (game_mode != previous_game_mode) and (game_state == previous_game_state):
+    #exitiing_mode == True
+    if (game_mode != previous_game_mode):
+        game_state_lies = True
+        print(game_state_lies)
+    previous_game_mode = game_mode
+    
+    if (game_state != previous_game_state):
+        game_state_lies = False
+    previous_game_state = game_state
+    
+    if ((game_mode != 'Exhibition') and (game_state_lies is False)):
+        exiting_tourney = True
+        if (game_state == 'open'):
+            if (new_match == 0):
                 os.system('cls')
                 bettor.set_balance(interactor.get_balance())
                 first_run = False
                 p1name = my_parser.get_p1name()
                 p2name = my_parser.get_p2name()
-                p1DB_ratings = bettor.set_player_rating(database.get_ratings_from_DB(p1name))
-                p2DB_ratings = bettor.set_player_rating(database.get_ratings_from_DB(p2name))
+                p1DB_ratings = bettor.set_player_rating(database.get_ratings_from_DB(p1name)) # Gets Mu and Sigma for Player 1 in DB, sets them to default if there are no prior matches in the DB, and sets them accordingly if there are.
+                p2DB_ratings = bettor.set_player_rating(database.get_ratings_from_DB(p2name)) # Gets Mu and Sigma for Player 2 in DB, sets them to default if there are no prior matches in the DB, and sets them accordingly if there are.
                 p1DB_streak = database.get_winstreaks_from_DB(p1name)
                 p2DB_streak = database.get_winstreaks_from_DB(p2name)
                 p1DB_odds = database.get_odds_average(p1name)
                 p2DB_odds = database.get_odds_average(p2name)
-                p1_probability = bettor.probability_of_p1_win(p1DB_ratings.mu, p1DB_ratings.sigma, p2DB_ratings.mu,
-                                                              p2DB_ratings.sigma)
-                predicted_winner = bettor.prediction(p1DB_ratings.mu, p2DB_ratings.mu, p1_probability, p1name,
-                                                     p2name)
-                kelly_bet = bettor.kelly_bet(p1_probability, p1DB_odds, p2DB_odds, bettor.balance, predicted_winner,
-                                             game_mode)
-                my_parser.gamemode_printer(p1name, p2name, p1DB_odds, p2DB_odds, p1DB_ratings, p2DB_ratings,
-                                           p1DB_streak, p2DB_streak, p1_probability, bettor.balance)
-                bettor.bet_outcome_amount()
+                p1_probability = bettor.probability_of_p1_win(p1DB_ratings.mu, p1DB_ratings.sigma, p2DB_ratings.mu, p2DB_ratings.sigma)
+                predicted_winner = bettor.prediciton(p1DB_ratings.sigma, p2DB_ratings.sigma, p1DB_ratings.mu, p2DB_ratings.mu,p1_probability,p1name, p2name, p1DB_streak, p2DB_streak, p1DB_odds, p2DB_odds)
+                # predicted_winner = bettor.predicted_winner(p1DB_ratings.sigma, p2DB_ratings.sigma, p1_probability, p1name, p2name, p1DB_streak, p2DB_streak)
+                kelly_bet = bettor.kelly_bet(p1_probability, p1DB_odds, p2DB_odds, bettor.balance, predicted_winner, game_mode)
+                my_parser.gamemode_printer(p1name, p2name, p1DB_odds, p2DB_odds, p1DB_ratings, p2DB_ratings, p1DB_streak, p2DB_streak, p1_probability, bettor.balance)
+                bettor.bet_outcome_amount(game_state_lies)
                 interactor.place_bet_on_website(bettor.format_bet(predicted_winner, kelly_bet))
                 new_match = 1
-                my_socket.find_winstreak = True
-        elif game_state == 'locked':
-            if first_run is False:
-                if new_match == 1:
+                my_socket.find_winstreak = True            
+        elif (game_state == 'locked'):
+            if (first_run is False):
+                if (new_match == 1):
                     game_time.timer_start()
                     p1_odds = my_parser.get_p1odds()
                     p2_odds = my_parser.get_p2odds()
-                    my_parser.gamemode_printer(p1name, p2name, p1DB_odds, p2DB_odds, p1DB_ratings, p2DB_ratings,
-                                               p1DB_streak, p2DB_streak, p1_probability, bettor.balance)
+                    my_parser.gamemode_printer(p1name, p2name, p1DB_odds, p2DB_odds, p1DB_ratings, p2DB_ratings, p1DB_streak, p2DB_streak, p1_probability, bettor.balance)
                     print(f"True Odds are: ({p1_odds} : {p2_odds})")
                     new_match = 2
-        elif (game_state == '1') or (game_state == '2'):
-            if first_run is False:
-                if new_match == 2:
+        elif (game_state == '1') or (game_state == '2'): 
+            if (first_run is False):
+                if (new_match == 2):            
                     p1_win_status = my_parser.set_p1winstatus()
                     p2_win_status = my_parser.set_p2winstatus()
                     is_tourney = my_parser.is_tourney()
                     game_time.timer_snapshot()
-                    my_parser.gamemode_printer(p1name, p2name, p1DB_odds, p2DB_odds, p1DB_ratings, p2DB_ratings,
-                                               p1DB_streak, p2DB_streak, p1_probability, bettor.balance)
+                    my_parser.gamemode_printer(p1name, p2name, p1DB_odds, p2DB_odds, p1DB_ratings, p2DB_ratings, p1DB_streak, p2DB_streak, p1_probability, bettor.balance)
                     ratings_to_db = bettor.update_ranking_after(game_state, p1DB_ratings, p2DB_ratings)
-                    my_socket.adjust_winstreak(p1_win_status, p2_win_status, thread.true_p1_streak,
-                                               thread.true_p2_streak)
+                    my_socket.adjust_winstreak(p1_win_status, p2_win_status, thread.true_p1_streak, thread.true_p2_streak)
                     my_socket.adjust_tier(thread.true_tier)
                     bettor.bet_outcome(p1name, p2name, game_state)
-                    database.record_match(p1name, p1_odds, p1_win_status, p2name, p2_odds, p2_win_status,
-                                          my_socket.adj_p1winstreak, my_socket.adj_p2winstreak,
-                                          my_socket.adj_p1_tier, my_socket.adj_p2_tier, ratings_to_db[0].mu,
-                                          ratings_to_db[0].sigma, ratings_to_db[1].mu, ratings_to_db[1].sigma,
-                                          game_time.snapshot, bettor.outcome, is_tourney)
+                    database.record_match(p1name, p1_odds, p1_win_status, p2name, p2_odds, p2_win_status, my_socket.adj_p1winstreak, my_socket.adj_p2winstreak, my_socket.adj_p1_tier, my_socket.adj_p2_tier, ratings_to_db[0].mu, ratings_to_db[0].sigma, ratings_to_db[1].mu, ratings_to_db[1].sigma, game_time.snapshot, bettor.outcome, is_tourney)                    
                     panda.panda_to_csv(database.db_for_pandas())
                     # panda.latest_details(p1name)
                     new_match = 0
-
-    elif game_mode == 'Matchmaking':
+     
+    elif ((game_mode == 'Matchmaking') and (game_state_lies is True)):
+        game_state_lies = False
         new_match = 0
-
-    elif game_mode == "Exhibition":
-        if game_state == "open":
-            if new_match == 0:
+    
+    elif ((game_mode == "Exhibition") and (game_state_lies is False)):
+        if (game_state == "open"):
+            if (new_match == 0):
                 os.system('cls')
-                print(
-                    f"In Exhibition.  No bets are placed, and nothing is recorded.  {my_parser.get_matches_remaining()} matches left.")
+                print(f"In Exhibition.  No bets are placed, and nothing is recorded.  {my_parser.get_matches_remaining()} matches left.")
                 new_match = 1
-        elif game_state == "locked":
-            if new_match == 1:
-                print(
-                    f"In Exhibition.  No bets are placed, and nothing is recorded.  {my_parser.get_matches_remaining()} matches left.")
+        elif (game_state == "locked"):
+            if (new_match == 1):
+                print(f"In Exhibition.  No bets are placed, and nothing is recorded.  {my_parser.get_matches_remaining()} matches left.")
                 new_match = 2
         elif (game_state == "1") or (game_state == "2"):
-            if new_match == 2:
-                if exiting_tourney:
-                    database.record_match(p1name, p1_odds, p1_win_status, p2name, p2_odds, p2_win_status,
-                                          my_socket.adj_p1winstreak, my_socket.adj_p2winstreak,
-                                          my_socket.adj_p1_tier, my_socket.adj_p2_tier, ratings_to_db[0].mu,
-                                          ratings_to_db[0].sigma, ratings_to_db[1].mu, ratings_to_db[1].sigma,
-                                          game_time.snapshot, bettor.outcome, 1)
-
+            if (new_match == 2):
+                if exiting_tourney == True:
+                    database.record_match(p1name,p1_odds, p1_win_status, p2name, p2_odds, p2_win_status, my_socket.adj_p1winstreak, my_socket.adj_p2winstreak, my_socket.adj_p1_tier, my_socket.adj_p2_tier, ratings_to_db[0].mu, ratings_to_db[0].sigma, ratings_to_db[1].mu, ratings_to_db[1].sigma, game_time.snapshot, bettor.outcome, 1)
+                    game_state_lies = False
                     exiting_tourney = False
-                print(
-                    f"In Exhibition.  No bets are placed, and nothing is recorded.  {my_parser.get_matches_remaining()} matches left.")
+                print(f"In Exhibition.  No bets are placed, and nothing is recorded.  {my_parser.get_matches_remaining()} matches left.")
                 new_match = 0
-    elif game_mode == "Exhibition":
-        database.record_match(p1name, p1_odds, p1_win_status, p2name, p2_odds, p2_win_status,
-                              my_socket.adj_p1winstreak, my_socket.adj_p2winstreak, my_socket.adj_p1_tier,
-                              my_socket.adj_p2_tier, ratings_to_db[0].mu, ratings_to_db[0].sigma,
-                              ratings_to_db[1].mu, ratings_to_db[1].sigma, game_time.snapshot, bettor.outcome, 1)
-
+    elif ((game_mode == "Exhibition") and (game_state_lies is True)):
+        database.record_match(p1name, p1_odds, p1_win_status, p2name, p2_odds, p2_win_status, my_socket.adj_p1winstreak, my_socket.adj_p2winstreak, my_socket.adj_p1_tier, my_socket.adj_p2_tier, ratings_to_db[0].mu, ratings_to_db[0].sigma, ratings_to_db[1].mu, ratings_to_db[1].sigma, game_time.snapshot, bettor.outcome, 1)
+        game_state_lies = False
         new_match = 0
 
 # NOTE: GENERAL QUESTIONS / THINGS.
